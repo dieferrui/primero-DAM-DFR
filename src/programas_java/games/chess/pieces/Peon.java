f21d1e4a7ba27b62859c676ffb92dec242a4f7b2
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2024-03-06 14:31:42 +0100

    On branch main  Your branch is ahead of 'origin/main' by 3 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   src/programas_java/games/chess/pieces/Peon.java
    	modified:   src/programas_java/games/chess/pieces/Tower.java

--- src/programas_java/games/chess/pieces/Peon.java
+++ src/programas_java/games/chess/pieces/Peon.java
@@ -13,26 +13,26 @@ public class Peon extends Piece {
 
     public ArrayList<Square> movePiece(Square square, ChessBoard board) {
 
-        int filPos = square.getFil();
-        int colPos = square.getCol();
         ArrayList<Square> validMoves;
 
         if (this.getColor().equals("white")) {
 
-            validMoves = moveIfWhite(square, board, filPos, colPos);
+            validMoves = moveIfWhite(square, board);
 
         } else {
 
-            validMoves = moveIfBlack(square, board, filPos, colPos);
+            validMoves = moveIfBlack(square, board);
 
         }
 
         return validMoves;
     }
 
-    private ArrayList<Square> moveIfWhite(Square square, ChessBoard board, int filPos, int colPos) {
+    private ArrayList<Square> moveIfWhite(Square square, ChessBoard board) {
 
         ArrayList<Square> validMoves = new ArrayList<>();
+        int filPos = square.getFil();
+        int colPos = square.getCol();
 
         if (square.getPiece().getNumberOfMoves() < 1 && board.boardLayout[filPos + 2][colPos].getPiece() == null &&
             board.boardLayout[filPos + 1][colPos].getPiece() == null) {
@@ -49,14 +49,14 @@ public class Peon extends Piece {
 
         }
 
-        if (board.boardLayout[filPos + 1][colPos - 1].getPiece() != null) {
+        if (board.boardLayout[filPos + 1][colPos - 1].getPiece().getColor().equals("black")) {
 
             Square eatLeft = board.boardLayout[filPos + 1][colPos - 1];
             validMoves.add(eatLeft);
 
         }
 
-        if (board.boardLayout[filPos + 1][colPos + 1].getPiece() != null) {
+        if (board.boardLayout[filPos + 1][colPos + 1].getPiece().getColor().equals("black")) {
 
             Square eatRight = board.boardLayout[filPos + 1][colPos + 1];
             validMoves.add(eatRight);
@@ -85,14 +85,14 @@ public class Peon extends Piece {
 
         }
 
-        if (board.boardLayout[filPos - 1][colPos - 1].getPiece() != null) {
+        if (board.boardLayout[filPos - 1][colPos - 1].getPiece().getColor().equals("white")) {
 
             Square eatLeft = board.boardLayout[filPos - 1][colPos - 1];
             validMoves.add(eatLeft);
 
         }
 
-        if (board.boardLayout[filPos - 1][colPos + 1].getPiece() != null) {
+        if (board.boardLayout[filPos - 1][colPos + 1].getPiece().getColor().equals("white")) {
 
             Square eatRight = board.boardLayout[filPos - 1][colPos + 1];
             validMoves.add(eatRight);
--- src/programas_java/games/chess/pieces/Tower.java
+++ src/programas_java/games/chess/pieces/Tower.java
@@ -1,5 +1,10 @@
 package programas_java.games.chess.pieces;
 
+import java.util.ArrayList;
+
+import programas_java.games.chess.ChessBoard;
+import programas_java.games.chess.Square;
+
 public class Tower extends Piece {
     
     public Tower(String color, String type) {
@@ -8,9 +13,119 @@ public class Tower extends Piece {
         
     }
 
-    public void movePiece() {
+    public ArrayList<Square> movePiece(Square square, ChessBoard board) {
 
-        // TODO
+        ArrayList<Square> validMoves;
 
+        validMoves = moveByColor(square, board, square.getPiece().getColor());
+
+        return validMoves;
+    }
+
+    public ArrayList<Square> moveByColor(Square square, ChessBoard board, String color) {
+
+        ArrayList<Square> validMoves = new ArrayList<>();
+        String targetColor;
+        int filPos = square.getFil();
+        int colPos = square.getCol();
+        
+        if (color.equals("white")) {
+
+            targetColor = "black";
+
+        } else {
+
+            targetColor = "white";
+        }
+        
+        // Movimiento vertical hacia arriba
+        for (int i = (filPos - 1); i >= 0; i--) {
+
+            if (board.boardLayout[i][colPos].getPiece() == null) {
+
+                validMoves.add(board.boardLayout[i][colPos]);
+
+            } else {
+
+                if (board.boardLayout[i][colPos].getPiece().getColor().equals(targetColor)) {
+
+                    validMoves.add(board.boardLayout[i][colPos]);
+                    break;
+
+                } else {
+
+                    break;
+
+                }
+            }
+        }
+
+        // Movimiento vertical hacia abajo
+        for (int i = (filPos + 1); i <= 8; i++) {
+
+            if (board.boardLayout[i][colPos].getPiece() == null) {
+
+                validMoves.add(board.boardLayout[i][colPos]);
+
+            } else {
+
+                if (board.boardLayout[i][colPos].getPiece().getColor().equals(targetColor)) {
+
+                    validMoves.add(board.boardLayout[i][colPos]);
+                    break;
+
+                } else {
+
+                    break;
+
+                }
+            }
+        }
+
+        // Movimiento horizontal a la izquierda
+        for (int i = (colPos - 1); i >= 0; i--) {
+
+            if (board.boardLayout[filPos][i].getPiece() == null) {
+
+                validMoves.add(board.boardLayout[filPos][i]);
+
+            } else {
+
+                if (board.boardLayout[filPos][i].getPiece().getColor().equals(targetColor)) {
+
+                    validMoves.add(board.boardLayout[filPos][i]);
+                    break;
+
+                } else {
+
+                    break;
+
+                }
+            }
+        }
+
+        // Movimiento horizontal a la derecha
+        for (int i = (colPos + 1); i <= 8; i++) {
+
+            if (board.boardLayout[filPos][i].getPiece() == null) {
+
+                validMoves.add(board.boardLayout[filPos][i]);
+
+            } else {
+
+                if (board.boardLayout[filPos][i].getPiece().getColor().equals(targetColor)) {
+
+                    validMoves.add(board.boardLayout[filPos][i]);
+                    break;
+
+                } else {
+
+                    break;
+
+                }
+            }
+        }
+
+        return validMoves;
     }
 }
