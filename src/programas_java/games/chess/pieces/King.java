ea568b4ad0cb33255f0bacb355e9ca1164d3f459
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2024-03-07 11:37:52 +0100

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/programas_java/games/chess/pieces/King.java
    	modified:   src/programas_java/games/chess/pieces/Queen.java

--- src/programas_java/games/chess/pieces/King.java
+++ src/programas_java/games/chess/pieces/King.java
@@ -1,5 +1,10 @@
 package programas_java.games.chess.pieces;
 
+import java.util.ArrayList;
+
+import programas_java.games.chess.ChessBoard;
+import programas_java.games.chess.Square;
+
 public class King extends Piece {
     
     public King(String color, String type) {
@@ -8,9 +13,86 @@ public class King extends Piece {
         
     }
 
-    public void movePiece() {
+    public ArrayList<Square> movePiece(Square square, ChessBoard board) {
 
-        // TODO
+        ArrayList<Square> validMoves = new ArrayList<>();
+        int filPos = square.getFil();
+        int colPos = square.getCol();
+        String targetColor;
 
+        if (square.getPiece().getColor().equals("white")) {
+
+            targetColor = "black";
+
+        } else {
+
+            targetColor = "white";
+        }
+
+        // Casilla a la izquierda
+        if (board.boardLayout[filPos][colPos - 1].getPiece() == null || 
+            board.boardLayout[filPos][colPos - 1].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos][colPos - 1]);
+
+        }
+
+        // Casilla a la derecha
+        if (board.boardLayout[filPos][colPos + 1].getPiece() == null || 
+            board.boardLayout[filPos][colPos + 1].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos][colPos + 1]);
+
+        }
+
+        // Casilla arriba
+        if (board.boardLayout[filPos - 1][colPos].getPiece() == null || 
+            board.boardLayout[filPos - 1][colPos].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos - 1][colPos]);
+
+        }
+
+        // Casilla abajo
+        if (board.boardLayout[filPos + 1][colPos].getPiece() == null || 
+            board.boardLayout[filPos + 1][colPos].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos + 1][colPos]);
+
+        }
+
+        // Casilla arriba a la izquierda
+        if (board.boardLayout[filPos - 1][colPos - 1].getPiece() == null || 
+            board.boardLayout[filPos - 1][colPos - 1].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos - 1][colPos - 1]);
+
+        }
+
+        // Casilla arriba a la derecha
+        if (board.boardLayout[filPos - 1][colPos + 1].getPiece() == null || 
+            board.boardLayout[filPos - 1][colPos + 1].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos - 1][colPos + 1]);
+
+        }
+
+        // Casilla abajo a la izquierda
+        if (board.boardLayout[filPos + 1][colPos - 1].getPiece() == null || 
+            board.boardLayout[filPos + 1][colPos - 1].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos + 1][colPos - 1]);
+
+        }
+
+        // Casilla abajo a la derecha
+        if (board.boardLayout[filPos + 1][colPos + 1].getPiece() == null || 
+            board.boardLayout[filPos + 1][colPos + 1].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos + 1][colPos + 1]);
+
+        }
+
+        return validMoves;
     }
 }
--- src/programas_java/games/chess/pieces/Queen.java
+++ src/programas_java/games/chess/pieces/Queen.java
@@ -1,5 +1,10 @@
 package programas_java.games.chess.pieces;
 
+import java.util.ArrayList;
+
+import programas_java.games.chess.ChessBoard;
+import programas_java.games.chess.Square;
+
 public class Queen extends Piece {
     
     public Queen(String color, String type) {
@@ -8,9 +13,210 @@ public class Queen extends Piece {
         
     }
 
-    public void movePiece() {
+    public ArrayList<Square> movePiece(Square square, ChessBoard board) {
 
-        // TODO
+        ArrayList<Square> validMoves = new ArrayList<>();
+        int filPos = square.getFil();
+        int colPos = square.getCol();
+        String targetColor;
 
+        if (square.getPiece().getColor().equals("white")) {
+
+            targetColor = "black";
+
+        } else {
+
+            targetColor = "white";
+        }
+
+        // Movimiento vertical hacia arriba
+        for (int i = (filPos - 1); i >= 0; i--) {
+
+            if (board.boardLayout[i][colPos].getPiece() == null) {
+
+                validMoves.add(board.boardLayout[i][colPos]);
+
+            } else {
+
+                if (board.boardLayout[i][colPos].getPiece().getColor().equals(targetColor)) {
+
+                    validMoves.add(board.boardLayout[i][colPos]);
+                    break;
+
+                } else {
+
+                    break;
+
+                }
+            }
+        }
+
+        // Movimiento vertical hacia abajo
+        for (int i = (filPos + 1); i <= 7; i++) {
+
+            if (board.boardLayout[i][colPos].getPiece() == null) {
+
+                validMoves.add(board.boardLayout[i][colPos]);
+
+            } else {
+
+                if (board.boardLayout[i][colPos].getPiece().getColor().equals(targetColor)) {
+
+                    validMoves.add(board.boardLayout[i][colPos]);
+                    break;
+
+                } else {
+
+                    break;
+
+                }
+            }
+        }
+
+        // Movimiento horizontal a la izquierda
+        for (int i = (colPos - 1); i >= 0; i--) {
+
+            if (board.boardLayout[filPos][i].getPiece() == null) {
+
+                validMoves.add(board.boardLayout[filPos][i]);
+
+            } else {
+
+                if (board.boardLayout[filPos][i].getPiece().getColor().equals(targetColor)) {
+
+                    validMoves.add(board.boardLayout[filPos][i]);
+                    break;
+
+                } else {
+
+                    break;
+
+                }
+            }
+        }
+
+        // Movimiento horizontal a la derecha
+        for (int i = (colPos + 1); i <= 7; i++) {
+
+            if (board.boardLayout[filPos][i].getPiece() == null) {
+
+                validMoves.add(board.boardLayout[filPos][i]);
+
+            } else {
+
+                if (board.boardLayout[filPos][i].getPiece().getColor().equals(targetColor)) {
+
+                    validMoves.add(board.boardLayout[filPos][i]);
+                    break;
+
+                } else {
+
+                    break;
+
+                }
+            }
+        }
+
+        // Movimiento diagonal arriba izquierda
+        for (int i = (filPos - 1); i >= 0; i--) {
+
+            for (int j = (colPos - 1); j >= 0; j--) {
+
+                if (board.boardLayout[i][j].getPiece() == null) {
+
+                    validMoves.add(board.boardLayout[i][j]);
+
+                } else {
+
+                    if (board.boardLayout[i][j].getPiece().getColor().equals(targetColor)) {
+
+                        validMoves.add(board.boardLayout[i][j]);
+                        break;
+
+                    } else {
+
+                        break;
+
+                    }
+                }
+            }
+        }
+
+        // Movimiento diagonal arriba derecha
+        for (int i = (filPos - 1); i >= 0; i--) {
+
+            for (int j = (colPos + 1); j <= 7; j++) {
+
+                if (board.boardLayout[i][j].getPiece() == null) {
+
+                    validMoves.add(board.boardLayout[i][j]);
+
+                } else {
+
+                    if (board.boardLayout[i][j].getPiece().getColor().equals(targetColor)) {
+
+                        validMoves.add(board.boardLayout[i][j]);
+                        break;
+
+                    } else {
+
+                        break;
+
+                    }
+                }
+            }
+        }
+
+        // Movimiento diagonal abajo izquierda
+        for (int i = (filPos + 1); i <= 7; i++) {
+
+            for (int j = (colPos - 1); j >= 0; j--) {
+
+                if (board.boardLayout[i][j].getPiece() == null) {
+
+                    validMoves.add(board.boardLayout[i][j]);
+
+                } else {
+
+                    if (board.boardLayout[i][j].getPiece().getColor().equals(targetColor)) {
+
+                        validMoves.add(board.boardLayout[i][j]);
+                        break;
+
+                    } else {
+
+                        break;
+
+                    }
+                }
+            }
+        }
+
+        // Movimiento diagonal abajo derecha
+        for (int i = (filPos + 1); i <= 7; i++) {
+
+            for (int j = (colPos + 1); j <= 7; j++) {
+
+                if (board.boardLayout[i][j].getPiece() == null) {
+
+                    validMoves.add(board.boardLayout[i][j]);
+
+                } else {
+
+                    if (board.boardLayout[i][j].getPiece().getColor().equals(targetColor)) {
+
+                        validMoves.add(board.boardLayout[i][j]);
+                        break;
+
+                    } else {
+
+                        break;
+
+                    }
+                }
+            }
+        }
+
+        return validMoves;
     }
 }
