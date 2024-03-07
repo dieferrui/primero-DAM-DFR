81c10a9166028661140a709e14ee98f12e7c8e1e
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2024-03-07 11:47:23 +0100

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/programas_java/games/chess/pieces/Horse.java

--- src/programas_java/games/chess/pieces/Horse.java
+++ src/programas_java/games/chess/pieces/Horse.java
@@ -1,5 +1,10 @@
 package programas_java.games.chess.pieces;
 
+import java.util.ArrayList;
+
+import programas_java.games.chess.ChessBoard;
+import programas_java.games.chess.Square;
+
 public class Horse extends Piece {
     
     public Horse(String color, String type) {
@@ -8,9 +13,87 @@ public class Horse extends Piece {
         
     }
 
-    public void movePiece() {
+    public ArrayList<Square> movePiece(Square square, ChessBoard board) {
 
-        // TODO
+        ArrayList<Square> validMoves = new ArrayList<>();
+        int filPos = square.getFil();
+        int colPos = square.getCol();
+        String targetColor;
 
+        if (square.getPiece().getColor().equals("white")) {
+
+            targetColor = "black";
+
+        } else {
+
+            targetColor = "white";
+        }
+
+        // L superior izquierda
+        if (board.boardLayout[filPos - 1][colPos - 2].getPiece() == null || 
+            board.boardLayout[filPos - 1][colPos - 2].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos - 1][colPos - 2]);
+
+        }
+
+        // L superior invertida izquierda
+        if (board.boardLayout[filPos - 2][colPos - 1].getPiece() == null || 
+            board.boardLayout[filPos - 2][colPos - 1].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos - 2][colPos - 1]);
+
+        }
+
+        // L superior invertida derecha
+        if (board.boardLayout[filPos - 2][colPos + 1].getPiece() == null || 
+            board.boardLayout[filPos - 2][colPos + 1].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos - 2][colPos + 1]);
+
+        }
+
+        // L superior derecha
+        if (board.boardLayout[filPos - 1][colPos + 2].getPiece() == null || 
+            board.boardLayout[filPos - 1][colPos + 2].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos - 1][colPos + 2]);
+
+        }
+
+        // L inferior invertida izquierda
+        if (board.boardLayout[filPos + 1][colPos - 2].getPiece() == null || 
+            board.boardLayout[filPos + 1][colPos - 2].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos + 1][colPos - 2]);
+
+        }
+
+        // L inferior izquierda
+        if (board.boardLayout[filPos + 2][colPos - 1].getPiece() == null || 
+            board.boardLayout[filPos + 2][colPos - 1].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos + 2][colPos - 1]);
+
+        }
+
+        // L inferior derecha
+        if (board.boardLayout[filPos + 2][colPos + 1].getPiece() == null || 
+            board.boardLayout[filPos + 2][colPos + 1].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos + 2][colPos + 1]);
+
+        }
+
+        // L inferior invertida derecha
+        if (board.boardLayout[filPos + 1][colPos + 2].getPiece() == null || 
+            board.boardLayout[filPos + 1][colPos + 2].getPiece().getColor().equals(targetColor)) {
+
+            validMoves.add(board.boardLayout[filPos + 1][colPos + 2]);
+
+        }
+
+        return validMoves;
     }
 }
+
