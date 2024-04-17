136bcaae4ecca797e5e5532578d695ce18c8c834
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2024-04-17 13:59:40 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   proyecto_iterables/proyecto_iterables/bin/Main.class
    	modified:   proyecto_iterables/proyecto_iterables/src/Main.java

--- proyecto_iterables/proyecto_iterables/src/Main.java
+++ proyecto_iterables/proyecto_iterables/src/Main.java
@@ -4,21 +4,23 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        // Ejercicio 1
         IteraLista.iterarNormal();
 
+        // Ejercicio 2
         Aula alumnos = new Aula<>();
-        Iterator iterador = alumnos.iterator();
-
+        
         alumnos.anadirAlumno(new Alumno("Diego", "123A", 27, "DAM", 10));
         alumnos.anadirAlumno(new Alumno("Alexandr", "456B", 19, "DAM", 9));
         alumnos.anadirAlumno(new Alumno("Alejandro", "789C", 19, "DAM", 8));
         alumnos.anadirAlumno(new Alumno("Nacho", "066D", 19, "DAM", 9));
 
+        Iterator iterador = alumnos.iterator();
 
         while (iterador.hasNext()) {
 
             System.out.println(iterador.next());
-            
+
         }
     }
 }
