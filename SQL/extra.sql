dc1eed466d703ab556ac5da12aa712d10dbb0725
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2024-05-09 08:08:31 +0200

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   SQL/extra.sql 	modified:   SQL/t8p1.sql

--- SQL/extra.sql
+++ SQL/extra.sql
@@ -84,3 +84,79 @@ where 	dorsal in (select   dorsal
 		   where    nomeq = 'Banesto');
 		   
 --18
+select 	P.nompuerto, P.netapa, E.km
+from 	puerto P, etapa E
+where 	P.netapa = E.netapa;
+
+--19
+select 	distinct E.nomeq, E.director
+from 	equipo E, ciclista C
+where  	C.nomeq = E.nomeq and
+	C.edad > 33;
+	
+--20
+select 	distinct C.nombre, M.color
+from 	ciclista C, maillot M, llevar L
+where 	C.dorsal = L.dorsal and
+	L.codigo = M.codigo
+order by C.nombre;
+
+--21
+select 	distinct C.nombre, E.netapa
+from 	ciclista C, llevar L, etapa E, maillot M
+where 	C.dorsal = E.dorsal and
+	L.netapa = E.netapa and
+	C.dorsal = L.dorsal and
+	L.codigo = M.codigo and 
+	lower(M.color) = 'amarillo';
+
+--22
+select 	E.netapa
+from 	etapa E, etapa E2
+where 	E.salida != E2.llegada and
+	E2.netapa = E.netapa - 1;
+
+--23
+select 	distinct netapa, salida
+from 	etapa
+where 	netapa not in   (select 	netapa
+			   from 	puerto)
+order by netapa;
+
+--24
+select 	round(avg(C.edad), 2) "Edad media"
+from 	ciclista C, etapa E
+where 	C.dorsal = E.dorsal;
+
+--25
+select 	nompuerto
+from 	puerto
+where 	altura >   (select   avg(altura)
+		    from     puerto);
+		    
+--26
+select 	E.salida, E.llegada
+from    etapa E, puerto P
+where 	E.netapa = P.netapa and
+	P.pendiente =   (select   max(pendiente)
+			 from     puerto);
+			 
+--27
+select 	C.nombre, P.dorsal
+from 	ciclista C, puerto P
+where 	C.dorsal = P.dorsal and
+	P.altura =   (select   max(altura)
+		      from     puerto);
+		      
+--28
+select 	nombre
+from  	ciclista
+where 	edad =   (select   min(edad)
+		  from     ciclista);
+		  
+--29
+select 	C.nombre
+from  	ciclista C, etapa E
+where 	C.dorsal = E.dorsal and
+	edad =   (select   min(edad)
+		  from     ciclista); --Incompleta
--- SQL/t8p1.sql
+++ SQL/t8p1.sql
@@ -12,16 +12,17 @@ having  count(*) > 15;
 --3
 select 	to_char(fecha, 'YYYY') "Año", count(*) "Núm. facturas"
 from 	factura
-group by to_char(fecha, 'YYYY');
+group by to_char(fecha, 'YYYY')
+order by to_char(fecha, 'YYYY') asc;
 
 --4
-select 	codloc, count(codpostal)
+select 	codloc, count(*)
 from 	cpostal
 where 	codloc like '3%'
 group by codloc;
 
 --5
-select 	max(count(codpostal))
+select 	max(count(*))
 from 	cpostal
 group by codloc;
 
@@ -33,7 +34,7 @@ having 	count(*) = (select   max(count(*))
 		    group by codloc);
 
 --6
-select 	codart, sum(linea * cant) "Cantidad vendida"
+select 	codart, sum(cant) "Cantidad vendida"
 from 	factura_linea
 where 	codart like 'F%'
 group by codart;
@@ -61,7 +62,8 @@ having  sum(cant * precio) > 6000;
 select 	codcli, iva, count(*) "Núm. facturas"
 from 	factura
 where  	codcli between 241 and 250
-group by codcli, iva;
+group by codcli, iva
+order by codcli asc;
 
 --10
 select 	codfac, sum(cant * precio) "Importe total"
@@ -103,3 +105,115 @@ where 	codven between 100 and 200 and
 	to_char(fecha, 'YYYY') = '2002'
 group by to_char(fecha, 'MM')
 order by to_char(fecha, 'MM');
+
+select 	min(iva)
+from 	factura
+where 	codven between 100 and 200 and
+	to_numbr(to_char(fecha, 'YYYY')) = to_number(to_char(sysdate, 'YYYY')) - 1
+group by to_char(fecha, 'MM')
+order by to_char(fecha, 'MM');
+
+--15
+select 	F.codcli, sum(FL.cant) "Núm. artículos" 
+from 	factura F, factura_linea FL
+where 	F.codfac = FL.codfac
+group by F.codcli
+having 	sum(FL.cant) >= 600;
+
+select 	F.codcli, C.nombre, sum(FL.cant) "Núm. artículos" 
+from 	factura F, factura_linea FL, cliente C
+where 	F.codfac = FL.codfac and
+	F.codcli = C.codcli
+group by F.codcli, C.nombre
+having 	sum(FL.cant) >= 600;
+
+--16
+select 	F.codcli, C.nombre, FL.codart, sum(FL.cant) "Núm. artículos" 
+from 	factura F, factura_linea FL, cliente C
+where 	F.codfac = FL.codfac and
+	C.codcli = F.codcli
+group by F.codcli, FL.codart, C.nombre
+having 	sum(FL.cant) >= 20;
+
+--17
+select 	C.nombre, C.codpostal, sum(FL.cant * FL.precio) "Facturación"
+from 	cliente C, factura_linea FL, factura F
+where 	C.codcli = F.codcli and
+	F.codfac = FL.codfac
+group by F.codcli, C.nombre, C.codpostal
+having 	sum(FL.cant * FL.precio) >= 6000;
+
+--18
+select 	F.codven, V.nombre, count(distinct FL.codart) "Artículos diferentes"
+from 	factura F, factura_linea FL, vendedor V
+where 	V.codven = F.codven and
+	F.codfac = FL.codfac and
+	lower(FL.codart) like 'b%'
+group by F.codven, V.nombre
+having 	count(distinct FL.codart) >= 8;
+
+--19
+select 	C.nombre, C.direccion, count(*) "Facturas año 2002"
+from 	cliente C, cpostal CP, provincia P, factura F, localidad L
+where 	C.codcli = F.codcli and
+	C.codpostal = CP.codpostal and
+	CP.codloc = L.codloc and
+	L.codprov = P.codprov and
+	P.nombre = 'CASTELLON' and
+	to_char(fecha, 'YYYY') = '2002'
+group by F.codcli, C.nombre, C.direccion;
+
+--20
+select 	P.nombre, count(*) "Clientes"
+from 	provincia P, localidad L, cpostal CP, cliente C
+where 	P.codprov = L.codprov and
+	L.codloc = CP.codloc and
+	CP.codpostal = C.codpostal
+group by L.codprov, P.nombre
+having 	count(*) > 5;
+
+--21
+select 	F.codcli, C.nombre, sum(cant * precio) "Cantidad gastada"
+from 	cliente C, factura F, factura_linea FL
+where 	C.codcli = F.codcli and
+	F.codfac = FL.codfac
+group by F.codcli, C.nombre, F.fecha
+having 	to_char(F.fecha, 'MM/YYYY') = '12/2002' and
+	sum(cant * precio) > 60
+order by codcli asc;
+
+--22
+select 	P.nombre "Provincia", L.nombre "Localidad", count(*) "Número de clientes"
+from 	localidad L, provincia P, cpostal CP, cliente C
+where 	L.codprov = P.codprov and
+	L.codloc = CP.codloc and
+	CP.codpostal = C.codpostal and
+	((P.codprov like '12%') or (P.codprov like '46%') or (P.codprov like '03%'))
+group by L.nombre, P.nombre
+order by P.nombre;
+
+--23
+select 	max(count(*)) "Máximos clientes"
+from 	localidad L, cpostal CP, cliente C
+where 	L.codloc = CP.codloc and
+	CP.codpostal = C.codpostal
+group by L.nombre;
+
+select 	CP.codloc, count(*) "Máximos clientes"
+from 	cpostal CP, cliente C
+where 	CP.codpostal = C.codpostal
+group by CP.codloc
+having 	count(*) =   (select   max(count(*))
+		      from     cpostal CP2, cliente C2
+		      where    CP2.codpostal = C2.codpostal
+		      group by CP2.codloc);
+		      
+--24
+select 	V.nombre, F.codven, count(*) "Máximo de facturas"
+from 	vendedor V, factura F
+where 	V.codven = F.codven
+group by F.codven, V.nombre
+having 	count(*) =   (select   max(count(*))
+		      from     vendedor V, factura F
+		      where    V.codven = F.codven
+		      group by V.codven);
