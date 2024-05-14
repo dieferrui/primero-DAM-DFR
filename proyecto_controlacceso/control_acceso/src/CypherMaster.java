844ec231673b6cfb65651cb079e140dc0762c711
dieferrui <dieferrui@alu.edu.gva.es>
2024-05-14 12:45:34 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   control_de_acceso/.gitattributes
    	new file:   control_de_acceso/.gitignore
    	new file:   control_de_acceso/app/build.gradle
    	new file:   control_de_acceso/app/src/main/java/control_de_acceso/App.java
    	new file:   control_de_acceso/app/src/test/java/control_de_acceso/AppTest.java
    	new file:   control_de_acceso/gradle/wrapper/gradle-wrapper.jar
    	new file:   control_de_acceso/gradle/wrapper/gradle-wrapper.properties
    	new file:   control_de_acceso/gradlew
    	new file:   control_de_acceso/gradlew.bat
    	new file:   control_de_acceso/settings.gradle
    	new file:   proyecto_controlacceso/control_acceso/src/CypherMaster.java
    	deleted:    proyecto_controlacceso/control_acceso/src/PlataformaAcceso.java

---
+++ proyecto_controlacceso/control_acceso/src/CypherMaster.java
@@ -0,0 +1,12 @@
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Security;
+import java.util.Base64;
+
+public class CypherMaster {
+    
+}
