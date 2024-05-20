a64da3aad577bf3cc0f59d5b3bd5a6aa4067bf2e
dieferrui <dieferrui@alu.edu.gva.es>
2024-05-20 08:30:21 +0200

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   .vscode/launch.json 	modified:   proyecto_iterables/proyecto_iterables/src/Main.java

--- proyecto_iterables/proyecto_iterables/src/Main.java
+++ proyecto_iterables/proyecto_iterables/src/Main.java
@@ -12,10 +12,10 @@ public class Main {
         // Ejercicio 2
         Aula alumnos = new Aula<>();
         
-        alumnos.anadirAlumno(new Alumno("Diego", "123A", 27, "DAM", 10));
         alumnos.anadirAlumno(new Alumno("Alexandr", "456B", 19, "DAM", 9));
         alumnos.anadirAlumno(new Alumno("Alejandro", "789C", 19, "DAM", 8));
         alumnos.anadirAlumno(new Alumno("Nacho", "066D", 19, "DAM", 9));
+        alumnos.anadirAlumno(new Alumno("Diego", "123A", 27, "DAM", 10));
 
         Iterator iterador = alumnos.iterator();
 
